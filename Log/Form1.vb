d3bcb9a8bde0b13e7f1108d0b9bf38c88203f9fa
Luis Reyes <luis_a_reyes13@hotmail.com>
2024-02-06 21:54:56 -0600

    Se protegen cambios

---
+++ Log/Form1.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Bitacora("Prueba de mensaje de error", eTipoMensaje.eError)
+        Bitacora("Prueba de mensaje de advertencia", eTipoMensaje.eAdvertencia)
+        Bitacora("Prueba de mensaje de informacion", eTipoMensaje.eInfomracion)
+    End Sub
+End Class
---
+++ Log/Principal.vb
@@ -0,0 +1,42 @@
+﻿Imports System.IO
+
+Module Principal
+    Public sRutaAplicacion As String = Environment.CurrentDirectory
+
+    Public Enum eTipoMensaje
+        eError = 1
+        eAdvertencia = 2
+        eInfomracion = 3
+    End Enum
+
+    Public Sub Bitacora(ByVal sMensaje As String, ByVal oTipo As eTipoMensaje)
+
+        Dim oDate As String
+
+        Try
+            oDate = Format(Date.Today, "yyyyMMdd")
+
+            If Not Directory.Exists(sRutaAplicacion & "\Log") Then
+                Directory.CreateDirectory(sRutaAplicacion & "\Log")
+            End If
+
+            If Not File.Exists(sRutaAplicacion & $"\Log\Proyecto_Log_{oDate}.txt") Then
+                Using oLogs As StreamWriter = New StreamWriter(sRutaAplicacion & $"\Log\Proyecto_Log_{oDate}.txt")
+                    oLogs.WriteLine($"Logs Generados el {Date.Now}")
+                    oLogs.WriteLine()
+                    oLogs.WriteLine("    Día        Hora          Tipo de Mensaje           Descripción")
+                    oLogs.WriteLine("------------------------------------------------------------------------")
+                    oLogs.Close()
+                End Using
+            End If
+
+            Using oLogs As StreamWriter = File.AppendText(sRutaAplicacion & $"\Log\Proyecto_Log_{oDate}.txt")
+                oLogs.WriteLine(Date.Now & If(oTipo = eTipoMensaje.eAdvertencia Or oTipo = eTipoMensaje.eInfomracion, "      ", "        ") &
+                                oTipo.ToString & If(oTipo = eTipoMensaje.eAdvertencia Or oTipo = eTipoMensaje.eInfomracion, "       ", "           ") & sMensaje)
+            End Using
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Module
